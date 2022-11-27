73445ad6430a38ee70659c5b4d98573a1540bd29
Sabrina S <112651522+sbr1nas@users.noreply.github.com>
2022-11-27 19:04:41 +0000

    finishing up part two of tutorial;

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
+import React, { Component, useState } from "react";
+import Support from "./components/Support";
+import ListCast from "./components/ListCast";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
+  let [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <h1>Meet the Stargzers!</h1>
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <ListCast
+            onChoice={(info) => {
+              setMemberInfo(info);
+            }}
+          />
+          {memberInfo && (
+            <article>
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  <img
+                    style={{ width: "200px" }}
+                    src={`images/${memberInfo.slug}.svg`}
+                    alt={memberInfo.name}
+                  />
+                  <hgroup>
+                    <h1>{memberInfo.name}</h1>
+                    <p>{memberInfo.bio}</p>
+                  </hgroup>
+                </div>
+              </>
+            </article>
+          )}
           <Support />
         </hgroup>
       </article>
     </div>
-  )
+  );
 }
-export default App
+export default App;
