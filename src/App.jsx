151e9016dc7e5ac4f35dccfeb3dc82907f39ba23
Sabrina S <112651522+sbr1nas@users.noreply.github.com>
2022-11-27 14:53:31 +0000

    completed lesson one - personal changes included

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 function App() {
+  const name = "StarGazers";
+  const styles = { color: "steelblue", fontStyle: "italic" };
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+    <div className="container">
+      <article>
+        <>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>
+            Meet the <b style={styles}>{name}</b>
+          </h1>
+          <p>
+            Members of an <b>intergalactic alliance</b> paving the way for peace
+            and benevolence among all species. They are known for their
+            enthusiasm for science, for their love of fun, and their dedication
+            to education.
+          </p>
+          <button className="outline" onClick={() => alert("Hi there")}>
+            Click Me
+          </button>
+        </>
+      </article>
+    </div>
+  );
 }
-export default App
+export default App;
